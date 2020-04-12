8830db6ff66cc57338b870d60b9d43c8429071ee
noahmmcgivern <noah.m.mcgivern@gmail.com>
2020-04-12 12:46:36 -0400

    Experience exhibits.

--- src/Asset.elm
+++ src/Asset.elm
@@ -1,12 +1,81 @@
-module Asset exposing (elm, linkedIn, logo, octocat)
+module Asset exposing
+    ( descBackend
+    , descCloud
+    , descFrontend
+    , descSecurity
+    , descSystems
+    , elm
+    , linkedIn
+    , logo
+    , octocat
+    )
 
 -- Asset
 -- These are things to include on the page.
+
+import Html exposing (Html, br, text)
 
 
 website : String
 website =
     "https://noahmcgivern.com"
+
+
+
+-- DESCRIPTIONS
+
+
+descriptionParagraph : String -> List (Html msg)
+descriptionParagraph description =
+    List.intersperse (br [] []) (List.map text (String.lines description))
+
+
+descBackend : List (Html msg)
+descBackend =
+    descriptionParagraph
+        """I deliver invaluable information through REST / SOAP APIs built using .NET Framework (C#/F#),
+
+            node.js plus additional frameworks like Serverless or Nest.js (JavaScript/TypeScript),
+
+            Spring Framework (Java/Kotlin/Scala),
+
+            and Django (Python)."""
+
+
+descCloud : List (Html msg)
+descCloud =
+    descriptionParagraph
+        """I carry cloud competence through practice in production environments primarily using Amazon Web Services and Microsoft Azure.
+            
+            No matter the challenge at hand, I am able to engineer simple solutions using many of the popular, modern PaaS providers."""
+
+
+descFrontend : List (Html msg)
+descFrontend =
+    descriptionParagraph
+        """I am comfortable creating user experiences with popular JavaScript node.js frameworks like React, Angular, Vue, Gatsby, Next.
+            
+            I also enjoy exploring the cutting-edge frontiers of JavaScript by studying TypeScript, Elm and PureScript."""
+
+
+descSecurity : List (Html msg)
+descSecurity =
+    descriptionParagraph
+        """Ethical hacking is a hobby of mine.
+            
+            I enjoy learning and teaching on topics like Bad USBs, Fuzz Testing/Exploit Development and Computer Forensics with Volatility.
+            
+            I apply it to my current work by implementing security functionality into our organization's development pipeline
+            
+            and when I'm charged with the responsibilty of Incident Response for our API services."""
+
+
+descSystems : List (Html msg)
+descSystems =
+    descriptionParagraph
+        """I build cloud development and production environments with IaC: Docker/Kubernetes, HashiCorp Stack, CloudFormation and ARM templates.
+            
+            I thoroughly enjoy virtualization and even have a home lab to experiment with these different technologies."""
 
 
 
--- src/Main.elm
+++ src/Main.elm
@@ -5,10 +5,13 @@ module Main exposing (main)
 
 import Asset
 import Bootstrap.CDN as CDN
+import Bootstrap.Card as Card
+import Bootstrap.Card.Block as Block
 import Bootstrap.Grid as Grid
 import Bootstrap.Grid.Col as Col
 import Bootstrap.Utilities.Spacing as Spacing
 import Browser
+import FontAwesome exposing (aws, cubes, docker, draftingCompass, icon, userShield)
 import Html exposing (..)
 import Html.Attributes as Attr
 
@@ -56,6 +59,16 @@ update msg model =
 -- VIEW
 
 
+descriptionSrc : List ( FontAwesome.Icon, String, List (Html msg) )
+descriptionSrc =
+    [ ( draftingCompass, "Front-end", Asset.descFrontend )
+    , ( cubes, "Back-end", Asset.descBackend )
+    , ( docker, "Systems", Asset.descSystems )
+    , ( aws, "Cloud", Asset.descCloud )
+    , ( userShield, "Security", Asset.descSecurity )
+    ]
+
+
 imageSrc : List ( String, String )
 imageSrc =
     [ ( "https://www.linkedin.com/in/noah-mcgivern/", Asset.linkedIn )
@@ -64,9 +77,32 @@ imageSrc =
     ]
 
 
+description : ( FontAwesome.Icon, String, List (Html msg) ) -> Grid.Column msg
+description ( faicon, title, desc ) =
+    Grid.col [ Col.md4, Col.sm12, Col.attrs [ Attr.style "position" "relative", Attr.style "margin-bottom" "15px" ] ]
+        [ Card.config [ Card.attrs [ Attr.style "height" "100%" ] ]
+            |> Card.headerH1 [ Attr.class "text-center" ]
+                [ icon faicon
+                , h4 [] [ text title ]
+                ]
+            |> Card.block []
+                [ Block.text [] desc ]
+            |> Card.view
+        ]
+
+
 image : ( String, String ) -> Grid.Column msg
 image ( link, asset ) =
-    Grid.col [ Col.md4, Col.sm12, Col.attrs [ Attr.style "position" "relative" ] ]
+    Grid.col
+        [ Col.md4
+        , Col.sm12
+        , Col.attrs
+            [ Attr.style "position" "relative"
+            , Attr.style "background-color" "white"
+            , Attr.style "border-radius" "10%"
+            , Attr.style "margin-bottom" "15px"
+            ]
+        ]
         [ a [ Attr.href link ]
             [ img
                 [ Attr.src asset
@@ -93,7 +129,7 @@ logo =
                 , Attr.style "display" "block"
                 , Attr.style "margin-left" "auto"
                 , Attr.style "margin-right" "auto"
-                , Attr.style "width" "600px"
+                , Attr.style "width" "500px"
                 , Attr.style "maxWidth" "100%"
                 ]
                 []
@@ -103,9 +139,10 @@ logo =
 
 
 view : Model -> Html Msg
-view model =
+view _ =
     Grid.container []
         [ CDN.stylesheet -- creates an inline style node with the Bootstrap CSS
         , Grid.row [] <| logo
+        , Grid.row [] <| List.map description descriptionSrc
         , Grid.row [] <| List.map image imageSrc
         ]
